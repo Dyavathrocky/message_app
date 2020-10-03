1add1c854268ac8926e22be4099cb3f6d3a97aee
Dyavathrocky <davathrak@gmail.com>
2020-10-03 19:46:39 +0530

    Message_app first commit

---
+++ posts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Post(models.Model):
+    text = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.text
+# Create your models here.
